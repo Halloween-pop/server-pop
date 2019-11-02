4cc25c26a47015e415aa0a3dd93619a37a009bab
ooeunz <yuns994@gmail.com>
2019-11-03 02:53:17 +0900

    score

---
+++ routes/api/result/scores.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+const au = require('../../../modules/util/authUtil');
+const rm = require('../../../modules/util/responseMessage');
+const sc = require('../../../modules/util/statusCode');
+
+const Score = require('../../../models/Score');
+
+router.put('/', async (req, res) => {
+    // const userIdx = req.session.userIdx;    // session
+    const {stage, scores, userIdx} = req.body;
+    console.log(scores)
+    console.log(userIdx);
+    
+
+    // TODO 1: blogName 값 확인하기
+    if (!stage || !score || !userIdx) res.status(sc.BAD_REQUEST).send(au.successFalse(rm.NULL_VALUE));
+
+    // TODO 2: 작성하기
+    try {
+        const {code, json} = await Score.insert(stage, scores, userIdx);
+        res.status(code).send(json);
+    } catch (err) {
+        console.log(err);
+        res.status(sc.INTERNAL_SERVER_ERROR).send(au.successFalse(rm.SCORE_CREATE_FAIL));
+    }
+});
+
+// READ_ALL
+router.get('/:userIdx', async (req, res) => {
+    const {userIdx} = req.params;
+
+    // TODO 1: 읽어오기
+    try {
+        const {code, json} = await Score.selectAll(userIdx);
+        res.status(code).send(json);
+        
+    } catch (err) {
+        console.log(err);
+        res.status(sc.INTERNAL_SERVER_ERROR).send(au.successFalse(rm.SCORE_READ_FAIL));
+    }
+});
+
+
+module.exports = router;
